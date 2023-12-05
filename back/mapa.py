5ea5b880ad60ebbefb462b37ec54660e7da653bd
JPortoL <jporto@unal.edu.co>
2023-12-05 02:05:10 -0500

    primeros pasos

---
+++ back/mapa.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import plotly.graph_objs as go
+import geopandas as gpd
+import plotly.offline as pyo
+import json
+from Constantes import DEPARTAMENTOS_INVERTIDO
+
+"""## Importar json con las ubicaciones"""
+import json
+
+def encontrar_llave(diccionario, valor_buscado):
+    for llave, valor in diccionario.items():
+        if valor == valor_buscado:
+            return llave
+    return None
+
+geojson_path = "../MUNICIPIOSREDDIT/dataset/MunicipiosModificados.geojson"
+
+with open(geojson_path, 'r', encoding='utf-8') as geojson_file:
+        geojson__ = json.load(geojson_file)
+    # Aquí puedes trabajar con el objeto GeoJSON
+
+"""## Cargar base de datos"""
+
+# Lee el GeoDataFrame desde el archivo GeoJSON
+gdf = gpd.read_file(geojson_path)
+
+"""## Crear mapa"""
+
+# Convierte el GeoDataFrame a un DataFrame de pandas
+df = pd.DataFrame({
+    "CDGNOMB": gdf["MPIO_CNMBR"] + ", " + gdf["DPTO_CCDGO"].map(DEPARTAMENTOS_INVERTIDO),
+    "COLOR": gdf["COLOR"]
+})
+for loc in geojson__["features"]:
+    loc["id"] = loc["properties"]["MPIO_CNMBR"] + ", " + DEPARTAMENTOS_INVERTIDO[loc["properties"]["DPTO_CCDGO"]] 
+# Ahora puedes usar directamente la geometría en formato GeoJSON
+fig = go.Figure(go.Choroplethmapbox(
+                  geojson=geojson__,
+                  locations=df['CDGNOMB'],
+                  z=df['COLOR'],
+                  colorscale="Greens",
+                  reversescale=True,
+                  marker=dict(line=dict(width=0.01, color='black')),
+                  showscale=False))
+fig.update_layout(mapbox_style="carto-positron",
+                        mapbox_zoom=3.4,
+                        mapbox_center = {"lat": 4.570868, "lon": -74.2973328},
+                        paper_bgcolor='rgba(0,0,0,0)', # Añade esta línea
+                        plot_bgcolor='rgba(0,0,0,0)', # Añade esta línea
+                        geo=dict(bgcolor= 'rgba(0,0,0,0)')) # Añade esta línea)
+
+fig.update_traces(
+    hovertemplate="<br>%{location}</br>" +
+                  "Estado: %{text}",
+    text=df['COLOR'].apply(lambda x: "ELIMINADO" if x == 1 else "CON VIDA"),
+)
+
+"""## Exportar en html"""
+
+pyo.plot(fig, filename='../MUNICIPIOSREDDIT/front/Mapa.html')
---
+++ back/test.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from Constantes import DEPARTAMENTOS
+
+def load_geodata(file_path):
+    data_geo = gpd.read_file(file_path)
+    data_geo["COLOR"] = 0  # Inicializar con 0 en lugar de crear una lista
+    return data_geo
+
+def cambiador_color(data_geo, df_eliminados):
+    for index in df_eliminados.index:
+            for column in df_eliminados.columns:
+                municipio = df_eliminados.at[index, column]
+                if pd.notna(municipio) and municipio.upper() in data_geo['MPIO_CNMBR'].str.upper().values:
+                    data = data_geo.loc[data_geo['MPIO_CNMBR'].str.upper() == municipio.upper(), 'COLOR']
+                    if data.values[0] == 1:
+                        print(municipio, "ya había sido eliminado")
+                    if len(data) > 1:
+                        print(municipio, f"está en {len(data)} departamentos")
+                    
+                    data_geo.loc[data_geo['MPIO_CNMBR'].str.upper() == municipio.upper(), 'COLOR'] = 1
+                elif pd.notna(municipio) and ("," in municipio):
+                    municipio, departamento = municipio.split(", ")
+                    municipio_upper = municipio.upper()
+                    data = data_geo.loc[(data_geo['MPIO_CNMBR'].str.upper() == municipio_upper) & (data_geo["DPTO_CCDGO"] == DEPARTAMENTOS[departamento.title()]), 'COLOR']
+                    if data.values[0] == 1:
+                        print(municipio, "ya había sido eliminado")
+                    data_geo.loc[(data_geo['MPIO_CNMBR'].str.upper() == municipio_upper) & (data_geo["DPTO_CCDGO"] == DEPARTAMENTOS[departamento.title()]), 'COLOR'] = 1
+                elif pd.notna(municipio):
+                    print(municipio, "no está en el Dataset")
+    
+    data_geo.to_file("../MUNICIPIOSREDDIT/dataset/MunicipiosModificados.geojson", driver='GeoJSON')
+
+def plot_colored_map(data_geo, dia):
+    color_dict = {0: 'limegreen', 1: 'white'}
+    cmap = colors.ListedColormap(list(color_dict.values()))
+    norm = colors.Normalize(vmin=0, vmax=1)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.title(f"Día {dia}")
+    data_geo.boundary.plot(ax=ax, color='black', linewidth=0.1)
+    data_geo.plot(column='COLOR', ax=ax, cmap=cmap, norm=norm)
+    plt.show()
+
+if __name__ == "__main__":
+    file_path = "../MUNICIPIOSREDDIT/dataset/MunicipiosVeredas19MB.json"
+    link_eliminados = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwZO7dV-5-s9J2op0T2ZZCrJ2yJ27keeGRBCYrlMa91Bwf2pt2mFbaHCDoFlx2_sziGQ8J_tfbKnFQ/pub?gid=0&single=true&output=csv"
+
+    data_geo = load_geodata(file_path)
+    df_eliminados = pd.read_csv(link_eliminados)
+
+    for municipio in df_eliminados[df_eliminados.columns[-1]]:
+        print(f"- {municipio.title()}" if pd.notna(municipio) else "")
+
+    cambiador_color(data_geo, df_eliminados)
+    plot_colored_map(data_geo, 3)
